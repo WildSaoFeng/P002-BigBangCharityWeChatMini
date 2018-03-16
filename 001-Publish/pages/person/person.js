92ca949f50c7cf241454e9050ee886b7258d6871
WildSaoFeng <1240730274@qq.com>
2018-03-16 15:17:28 +0800

    2018-03-16 15:17:34 FrontEnd Almost Finished.

---
+++ 001-Publish/pages/person/person.js
@@ -0,0 +1,85 @@
+// pages/person/person.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userLocation:{},
+    defaultLocation:{
+      longitute: 116.283696,
+      latitude: 40.170167,
+    },
+    defaultMarkers:[{
+      latitude: 40.170167,
+      longitude: 116.283696,
+      name: '中央财经大学沙河校区',
+      iconPath: '../../images/icon/page2-f.png',
+      desc: '现在位置',
+      width: 25,
+      height: 25,
+      alpha: 0.5,
+      label: {
+        content:'我的位置',
+        
+      },
+    }],
+    userHasLocation: false,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
--- 001-Publish/pages/profile/profile.js
+++ 001-Publish/pages/profile/profile.js
@@ -10,9 +10,14 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap1: function () {
     wx.navigateTo({
-      url: '../logs/logs'
+      url: '../good/good'
+    })
+  },
+  bindViewTap2: function () {
+    wx.navigateTo({
+      url: '../projectList/projectList'
     })
   },
   onLoad: function () {
@@ -21,7 +26,7 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -42,13 +47,5 @@ Page({
         }
       })
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
   }
 })
